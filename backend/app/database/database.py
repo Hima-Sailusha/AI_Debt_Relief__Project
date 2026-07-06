f62d0c0f7cf01370fe7118756d7e4bce9f47a01c
Hima-Sailusha <ayanampudihimasailushahimasail@gmail.com>
2026-07-06 23:07:57 +0530

    Add get_db dependency

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
 from backend.app.core.config import DATABASE_URL
 
 os.makedirs("database", exist_ok=True)
@@ -17,3 +18,12 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+# Dependency for FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
