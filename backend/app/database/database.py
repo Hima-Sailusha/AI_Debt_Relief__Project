775a2b04a3a04b4397f028845d8229e5a30ae447
Hima-Sailusha <ayanampudihimasailushahimasail@gmail.com>
2026-07-06 22:54:02 +0530

    Fix absolute imports for Render

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,7 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import DATABASE_URL
+from backend.app.core.config import DATABASE_URL
 
 os.makedirs("database", exist_ok=True)
 
